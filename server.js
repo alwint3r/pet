b606b4306b48ab6c814ad987d18703bb1b846625
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-12-04 11:16:48 +0700

    Modified server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const swig = require('swig');
-
 const config = require('./config');
 
-const indexRoute = require('./routes/');
+// db & swig config
 
+require('./db');
 swig.setDefaults(config.swig);
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 
+// routes config
+
+const indexRoute = require('./routes/');
+const petRoute = require('./routes/pet');
+
 app.use('/', indexRoute);
+app.use('/api/pet', petRoute);
 
 app.listen(config.port, () => {
   console.log(`Pet Server is listening on port ${config.port}`);
