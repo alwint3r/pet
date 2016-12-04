ff3eb49e9ccfa378316a6ff40cd0a9b61be0df00
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-12-04 14:10:12 +0700

    Moar operation

--- middlewares/pet.js
+++ middlewares/pet.js
@@ -1,8 +1,6 @@
 
 const mongoose = require('mongoose');
-
 require('../models/pet');
-
 const Pet = mongoose.model('Pet');
 
 // const Pet = require('../models/pet');
@@ -42,6 +40,96 @@ module.exports = {
             message: 'Pet saved',
           },
           data: inserted,
+        });
+      })
+      .catch(ex => {
+        return res.json({
+          meta: {
+            code: 500,
+            message: ex.message,
+          },
+        });
+      });
+  },
+
+  getOne(req, res, next) {
+    const id = req.params.id;
+    const query = {
+      _id: id,
+    };
+
+    Pet.findOne(query)
+      .then(result => {
+        if (!result) {
+          res.status(404);
+
+          return res.json({
+            meta: {
+              code: 404,
+              message: 'Pet is not found',
+            },
+          });
+        }
+
+        return res.json({
+          meta: {
+            code: 200,
+            message: 'Found pet',
+          },
+          data: result,
+        });
+      })
+      .catch(ex => {
+        return res.json({
+          meta: {
+            code: 500,
+            message: ex.message,
+          }
+        });
+      });
+  },
+
+  update(req, res, next) {
+    const update = req.body;
+    const query = {
+      _id: req.params.id,
+    };
+
+    Pet
+      .findOneAndUpdate(query, update)
+      .exec()
+      .then(() => {
+        return res.json({
+          meta: {
+            code: 200,
+            message: 'Pet is updated',
+          }
+        });
+      })
+      .catch(ex => {
+        return res.json({
+          meta: {
+            code: 500,
+            message: ex.message,
+          },
+        });
+      });
+  },
+
+  remove(req, res, next) {
+    const query = {
+      _id: req.params.id,
+    };
+
+    Pet
+      .findOneAndRemove(query)
+      .exec()
+      .then(() => {
+        return res.json({
+          meta: {
+            code: 200,
+            message: 'Pet is removed',
+          },
         });
       })
       .catch(ex => {
--- routes/pet.js
+++ routes/pet.js
@@ -5,5 +5,8 @@ const petMiddleware = require('../middlewares/pet');
 
 router.get('/', petMiddleware.list);
 router.post('/', petMiddleware.add);
+router.get('/:id', petMiddleware.getOne);
+router.put('/:id', petMiddleware.update);
+router.delete('/:id', petMiddleware.remove);
 
 module.exports = router;
