cf3ddd1ec07c74b5f32dd67b4ed061e7da889b15
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-12-04 08:03:59 +0700

    Added README.md and moar files

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
   db: 'mongodb://localhost/petnode',
   port: 8000,
+  swig: {
+    cache: false,
+  },
 };
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require(`express`);
+const express = require('express');
 const app = express();
+const swig = require('swig');
 
-const config = require(`./config`);
+const config = require('./config');
+
+const indexRoute = require('./routes/');
+
+swig.setDefaults(config.swig);
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
+app.use('/', indexRoute);
 
 app.listen(config.port, () => {
   console.log(`Pet Server is listening on port ${config.port}`);
